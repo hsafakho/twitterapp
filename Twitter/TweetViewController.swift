a1865a8599168a6db41cde1095e56c3035574d75
Hamidreza Safakhoo <50091982+hsafakho@users.noreply.github.com>
2020-02-11 21:41:05 -0800

    Added the TweetViewController but could not add in the class

---
+++ Twitter/TweetViewController.swift
@@ -0,0 +1,24 @@
+//
+//  TweetViewController.swift
+//  Twitter
+//
+//  Created by Kay Lab on 2/11/20.
+//  Copyright © 2020 Dan. All rights reserved.
+//
+
+import UIKit
+
+class TweetViewController: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
